a2c3dd28ef64374da3970949b43fa6c49efed38b
scream514 <aryankamboj145@gmail.com>
2020-08-17 16:26:54 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import re
+import os
+import resolve_abbrev
+
+x = "afaiuvbjaSDVS VOPDvjWOIVND  vdOIVHNJS vdOSAIHFVO vcAV"
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
